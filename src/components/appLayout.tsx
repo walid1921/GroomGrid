a4c0b54d9cf16eab1e702ccc9a25b57438fd27ae
user.walid1921 <user.kouiderayadwalid@gmail.com>
2024-06-09 03:25:23 +0200

    nav

--- src/components/appLayout.tsx
+++ src/components/appLayout.tsx
@@ -8,9 +8,9 @@ const AppLayout = () => {
     <div className="grid sm:grid-cols-[20rem,1fr] grid-rows-[auto,1fr] h-screen">
       <Header />
       <Sidebar />
-      <main className="sm:p-16 pt-[30px] px-6 bg-bgMain overflow-scroll h-screen sm:h-auto">
+      <main className="sm:p-16 pt-[120px] px-6 bg-bgMain overflow-scroll h-screen sm:h-auto">
         <div className="max-w-[120rem] mx-0 my-auto flex flex-col gap-14">
-          <nav className="sm:hidden flex justify-center bg-background py-2 rounded-md">
+          <nav className="sm:hidden flex justify-center bg-[rgba(114,114,114,.2)] backdrop-blur-md   border-[rgba(114,114,114,.4)] py-2 px-5 rounded-md fixed top-3 z-[1000]">
             <SmallNav />
           </nav>
           <Outlet />
