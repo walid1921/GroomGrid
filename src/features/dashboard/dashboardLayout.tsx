1f867accc905e815d31b0dbd33074200b8422c1a
user.walid1921 <user.kouiderayadwalid@gmail.com>
2024-06-29 00:53:12 +0200

    dashboard

--- src/features/dashboard/dashboardLayout.tsx
+++ src/features/dashboard/dashboardLayout.tsx
@@ -13,7 +13,7 @@ function DashboardLayout() {
   return (
     <div className="grid grid-cols-4 grid-rows-[auto_34rem_auto] gap-[2.4rem] ">
       <div>Statistics</div>
-      <div>Today's activity</div>
+      <div>Today's activities</div>
       <div>Chart stay duration</div>
       <div>Chart sales</div>
     </div>
