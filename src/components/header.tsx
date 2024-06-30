245299df98fd26d97d6a84b5a735a4fcf4150cce
user.walid1921 <user.kouiderayadwalid@gmail.com>
2024-07-01 01:27:58 +0200

    avatar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,8 @@ import { ModeToggle } from "./mode-toggle";
 
 const Header = () => {
   return (
-    <header className="py-[1.2rem] px-[4.8rem] bg-background border-b items-center justify-between hidden sm:flex">
-      <span>Header</span>
-      <div className="flex gap-4">
+    <header className="py-[1.2rem] px-[1rem] sm:px-[4.8rem] bg-background border-b ">
+      <div className="flex justify-end items-center gap-4">
         <Logout />
         <ModeToggle />
       </div>
